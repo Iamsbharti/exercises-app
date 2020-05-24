13030819f6548b59830a9d2e15d39ddcc1537d37
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-25 01:59:31 +0530

    added UI for main content

--- src/Components/Excercises/index.js
+++ src/Components/Excercises/index.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { Grid } from "@material-ui/core";
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
 export default props => {
+  const styles = {
+    Paper: { padding: 20, marginTop: 20, marginBottom: 20 }
+  };
   return (
     <div>
-      <code>Main excercise content</code>
+      <Grid container spacing="3">
+        <Grid item sm>
+          <LeftPane styles={styles} />
+        </Grid>
+        <Grid item sm>
+          <RightPane styles={styles} />
+        </Grid>
+      </Grid>
     </div>
   );
 };
